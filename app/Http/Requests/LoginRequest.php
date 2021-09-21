63f5738138f97042db82fd71afe792fd56d598e4
murage <murage@labskenya.com>
2021-09-21 16:07:38 +0300

    feat: add user login request validation rules

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'password' => 'required',
+            'email' => 'required_without:phone_number|email',
+            'phone_number' => 'required_without:email' //allow any phone number 4 demo seed data
+        ];
+    }
+}
