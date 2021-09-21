3ea0f09215c7bcfd3bc94706c48423d03e46b7be
murage <murage@labskenya.com>
2021-09-21 17:37:18 +0300

    feat: add api endpoints to register and authenticate a user

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::group(['prefix' => 'auth', 'namespace' => 'App\Http\Controllers\Auth', 'middleware' => 'guest'], function () {
+        Route::post('register', 'RegisteredUserController@store');
+        Route::post('login', 'AuthenticatedSessionController@store');
+    });
+
+
 });
